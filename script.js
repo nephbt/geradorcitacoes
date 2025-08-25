f0863db56d58c6f0988d51c8e781ebd237d22ece
Guilherme Abrão <bteles400@gmail.com>
2025-08-25 15:57:25 -0300

    feat: Adiciona mais citações à lista + Implementa lógica do botão para gerar citação aleatória

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const citacoes = [
  "A imaginação é mais importante que o conhecimento. - Albert Einstein",
+ "A vida é o que acontece enquanto você está ocupado fazendo outros planos. - John Lennon",
+ "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo. - Winston Churchill"
 ];
+
+const citacaoEl = document.getElementById("citacao");
+const botaoEl = document.getElementById("novaCitacaoBtn");
+botaoEl.addEventListener("click", () => {
+ const randomIndex = Math.floor(Math.random() * citacoes.length);
+ citacaoEl.textContent = citacoes[randomIndex];
+});
